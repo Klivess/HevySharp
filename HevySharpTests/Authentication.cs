82946ebe77364d92fd8fc7f31e6e1748cd7b0d16
Klives <klivesdev@gmail.com>
2026-03-06 22:25:03 +0000

    hevy sharp tests

---
+++ HevySharpTests/Authentication.cs
@@ -0,0 +1,21 @@
+﻿using HevySharp;
+
+namespace HevySharpTests
+{
+    public class Authentication
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Assert.That(string.IsNullOrEmpty(Environment.GetEnvironmentVariable("HEVYAPIKEY")) == false);
+        }
+
+        [Test]
+        public void LoginToHevy()
+        {
+            HevyAPI api = new HevyAPI();
+            string apiKey = Environment.GetEnvironmentVariable("HEVYAPIKEY");
+            Assert.That((api.AuthoriseHevy(apiKey).Result == true));
+        }
+    }
+}
