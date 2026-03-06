6adc861db94984fbba293b5e30c2a22d29f11762
Klives <klivesdev@gmail.com>
2026-03-06 22:19:26 +0000

    init, login to Hevy

---
+++ HevySharp/HevyAPI.cs
@@ -0,0 +1,29 @@
+﻿namespace HevySharp
+{
+    public class HevyAPI
+    {
+        private string baseURL = "https://api.hevyapp.com";
+        private string apiKey = "";
+        private HttpClient client;
+        private bool IsAuthorised = false;
+        public HevyAPI()
+        {
+            client = new();
+        }
+
+        public async Task<bool> AuthoriseHevy(string apiKey)
+        {
+            client.DefaultRequestHeaders.Add("api-key", apiKey);
+            var response = await client.GetAsync(baseURL + "/v1/user/info");
+            if (response.IsSuccessStatusCode)
+            {
+                IsAuthorised = true;
+                return true;
+            }
+            return false;
+        }
+
+
+
+    }
+}
